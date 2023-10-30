874bdaae9cfece013ca633fa15b4fb256fec934b
hp omen <hp omen@DESKTOP-I0EO1P5>
2023-10-29 20:28:41 -0600

    Agregar archivos de proyecto.

---
+++ Hola mundo/Act.2 Manejo de memoria estática/Act.2 Manejo de memoria estática/Act.2 Manejo de memoria estática.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+const int NUM = 8;
+int main()
+{
+	int nums[NUM];
+	int total = 0;
+	for (int i = 0; i < NUM; i++)
+	{
+		cout << "Por favor, introduzca el numero";
+		cin >> nums[i];
+		total += nums[i];
+		}
+	cout << "El total de número es" << total << endl; 
+	system("pause");
+
+	return 0;
+}
